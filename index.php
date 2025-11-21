76a69ff4ce67305a3e95b9d56b79301eff7a1450
unknown <sio1@c106-11.220056s.local>
2025-11-21 17:12:38 +0100

    projetphp

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>BLIBLIODRIVE</title>
+</head>
+<body>
+    <p>la blibliblibliblihotèque de moulinssart est temporairement fermé. vous pouvez retrouvver ci joint la réservation de vos livre favoris </p>
+ 	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-9">
+					
+          <div class="container mt-4">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Rechercher...">
+              <button class="btn btn-outline-secondary" type="button">
+                <i class="bi bi-search"></i>
+              </button>
+            </div>
+          </div>
+			</div>
+			<div class="col-sm-3">
+					image Moulinsart
+			</div>
+		</div>
+		<div class="row">
+		   <div class="col-sm-9">
+					carroussel / résultat de la recherche / pages d'admin (ajout d'un livre)
+			</div>
+			<div class="col-sm-3">
+					formulaire de connexion / profil connecté (include)
+			</div>
+		</div>
+	</div>
+</body>
+</html>
