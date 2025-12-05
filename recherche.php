746b62055410d9f6c5475313c78cb77e33b44a06
Noah22550 <noahdepagne@gmail.com>
2025-12-05 16:49:24 +0100

    vers du 5/12/25

--- recherche.php
+++ recherche.php
@@ -28,22 +28,6 @@ if (!empty($_GET['query'])) {
     $stmt = $connexion->prepare($sql);
     $stmt->execute([':q' => $q]);
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    if ($livres) {
-        echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
-        foreach ($livres as $l) {
-            echo '<div class="col">';
-            echo '<div class="card h-100">';
-            if ($l['photo']) echo '<img src="covers/'.$l['photo'].'" class="card-img-top" alt="'.htmlspecialchars($l['titre']).'">';
-            echo '<div class="card-body">';
-            echo '<h5 class="card-title">'.htmlspecialchars($l['titre']).'</h5>';
-            echo '<p class="card-text">'.htmlspecialchars($l['prenom'].' '.$l['nom']).'</p>';
-            echo '</div></div></div>';
-        }
-        echo '</div>';
-    } else {
-        echo '<p class="text-center">Aucun livre trouvé pour cet auteur.</p>';
-    }
 }
 ?>
 </div>
