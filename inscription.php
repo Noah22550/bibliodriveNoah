341b20cba7693d0f042a29bea5d0686e4aea4c85
Noah22550 <noahdepagne@gmail.com>
2025-12-09 17:08:02 +0100

    version du 09/12/2025

--- inscription.php
+++ inscription.php
@@ -9,35 +9,19 @@
 </head>
 <body>
   <section>
-  <?php
-  if (!isset($_POST['btnSeConnecter'])) { /* L'entrée btnSeConnecter est vide = le formulaire n'a pas été submit=posté, on affiche le formulaire */
-    echo '
-    <form action="authentification.php" method = "post">
-        nom: <input name="nom" type="text" size ="30"">
-        code : <input name="code" type="text" size ="30"">
-        <input type="submit" name="btnSeConnecter"  value="Se connecter">
-    </form>';
-} else
-/* L'utilisateur a cliqué sur Se connecter, l'entrée btnSeConnecter <> vide, on traite le formulaire */
-{
-// On se connecte
-    require_once 'connexion.php';
-    $nom = $_POST['nom'];
-    $code = $_POST['code'];
-    $stmt = $connexion->prepare("SELECT * FROM agent where nom=:pnom AND code=:pcode");
-    $stmt->bindValue(":pnom", $nom); // pas de troisième paramètre STR par défaut
-    $stmt->bindValue(":pcode", $code); // idem
-    $stmt->setFetchMode(PDO::FETCH_OBJ);
-// Les résultats retournés par la requête seront traités en 'mode' objet
-    $stmt->execute();
-    $enregistrement = $stmt->fetch(); // boucle while inutile
-    if ($enregistrement) { // si $enregistrement n'est pas vide = on a trouvé quelque chose -> on est connecté
-        echo '<h1>Connexion réussie !</h1>';
-        echo 'bienvenido '.$enregistrement->prenom;
-    } else { // La requête n'a pas retournée de résultat, on a pas trouvé de ligne correspondant au mel et mot de passe
-        echo "Echec à la connexion.";
-    }
-}
-  ?>
+  <section>
+    <h1>connexion</h1>
+    <div class="input-box">
+      <input type="text" id="new-username" placeholder="nom utilisateur">
+    </div>
+    <div class="input-box">
+      <input type="password" id="new-password" placeholder="mot de passe">
+    </div>
+    <button class="register-btn">se connecter</button>
+    <div class="login-link">
+      <p>Déjà un compte ? <a href="index.html">Se connecter</a></p>
+    </div>
+  </section>
+
 </body>
 </html>
