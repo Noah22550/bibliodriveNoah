207808bdab9747758cf41c4890ec39dddab88106
Noah22550 <noahdepagne@gmail.com>
2025-12-16 17:12:27 +0100

    16/12/2025

--- entete.php
+++ entete.php
@@ -2,7 +2,6 @@
 <head>
 <title>entete</title>
 <meta charset="utf-8">
-    <!-- charset=UTF-8 : pour que les caractères accentués ressortent correctement -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
